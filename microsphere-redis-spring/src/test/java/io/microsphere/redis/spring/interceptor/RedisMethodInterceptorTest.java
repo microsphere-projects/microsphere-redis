a280488e7f26044d84227fda6a80f981a546eebe
Mercy Ma <mercyblitz@gmail.com>
2025-12-04 18:47:39 +0800

    Initialize redisConnection in test setup
    Added assignment of redisConnection in the setUp method of RedisMethodInterceptorTest to ensure the connection is initialized before each test.

--- microsphere-redis-spring/src/test/java/io/microsphere/redis/spring/interceptor/RedisMethodInterceptorTest.java
+++ microsphere-redis-spring/src/test/java/io/microsphere/redis/spring/interceptor/RedisMethodInterceptorTest.java
@@ -62,6 +62,7 @@ class RedisMethodInterceptorTest extends AbstractRedisTest {
 
     @BeforeEach
     void setUp() {
+        this.redisConnection = redisConnectionFactory.getConnection();
         this.interceptor = new RedisMethodInterceptorImpl();
         this.context = new RedisMethodContext(this.redisConnection, SET_METHOD, SET_METHOD_ARGS, this.redisContext, SOURCE_BEAN_NAME_FOR_REDIS_TEMPLATE);
     }
