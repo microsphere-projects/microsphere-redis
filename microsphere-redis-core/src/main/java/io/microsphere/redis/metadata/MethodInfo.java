b2e902ac723033168c6f2436f203e23e98c57b80
Mercy Ma <mercyblitz@gmail.com>
2025-12-22 19:54:58 +0800

    Refactor MethodInfo from record to class
    Replaces the MethodInfo record with a standard class, adds explicit fields, constructors, getters, and overrides for equals, hashCode, and toString. This change provides more control over the class's behavior and prepares for future extensibility.

--- microsphere-redis-core/src/main/java/io/microsphere/redis/metadata/MethodInfo.java
+++ microsphere-redis-core/src/main/java/io/microsphere/redis/metadata/MethodInfo.java
@@ -19,6 +19,9 @@ package io.microsphere.redis.metadata;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Objects;
+
+import static io.microsphere.redis.util.RedisCommandUtils.buildMethodId;
 
 /**
  * The {@link Method} Information class
@@ -28,6 +31,69 @@ import java.util.List;
  * @see MethodMetadata
  * @since 1.0.0
  */
-public record MethodInfo(String id, Method method, MethodMetadata methodMetadata,
-                         List<ParameterMetadata> parameterMetadataList) {
+public class MethodInfo {
+
+    private final String id;
+
+    private final Method method;
+
+    private final MethodMetadata methodMetadata;
+
+    private final List<ParameterMetadata> parameterMetadataList;
+
+    public MethodInfo(Method method, MethodMetadata methodMetadata, List<ParameterMetadata> parameterMetadataList) {
+        this.id = buildMethodId(method);
+        this.method = method;
+        this.methodMetadata = methodMetadata;
+        this.parameterMetadataList = parameterMetadataList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getIndex() {
+        return this.methodMetadata.getIndex();
+    }
+
+    public String getName() {
+        return this.method.getName();
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public MethodMetadata getMethodMetadata() {
+        return methodMetadata;
+    }
+
+    public List<ParameterMetadata> getParameterMetadataList() {
+        return parameterMetadataList;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MethodInfo)) {
+            return false;
+        }
+
+        MethodInfo that = (MethodInfo) o;
+        return Objects.equals(this.id, that.id)
+                && Objects.equals(this.methodMetadata, that.methodMetadata)
+                && Objects.equals(this.parameterMetadataList, that.parameterMetadataList);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getIndex();
+    }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
 }
