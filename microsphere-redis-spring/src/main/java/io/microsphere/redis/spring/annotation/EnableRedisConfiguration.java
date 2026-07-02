ac6cafc8c312863638b152a0a8d1e25ce06b4e76
Mercy Ma <mercyblitz@gmail.com>
2026-07-02 23:21:28 +0800

    Make EnableRedisConfiguration inherited
    Add `@Inherited` to `@EnableRedisConfiguration` so subclasses can inherit the annotation behavior automatically. The annotation declaration was also cleaned up to use static imports for `@Target` and `@Retention` constants, improving readability without changing functionality.

--- microsphere-redis-spring/src/main/java/io/microsphere/redis/spring/annotation/EnableRedisConfiguration.java
+++ microsphere-redis-spring/src/main/java/io/microsphere/redis/spring/annotation/EnableRedisConfiguration.java
@@ -21,9 +21,14 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Meta-annotation that enables the {@link RedisConfiguration} Spring bean, which exposes
@@ -44,9 +49,10 @@ import java.lang.annotation.Target;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
-@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
 @Documented
+@Inherited
 @Import(RedisConfigurationBeanDefinitionRegistrar.class)
 public @interface EnableRedisConfiguration {
 }
